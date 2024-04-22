a698ef06a858a56df755afb25d42f678c52c9565
chaerlo127 <monynony0203@gmail.com>
2024-04-22 03:55:52 +0000

    feat: 비즈니스 로직 수정 - 페이징, 삭제, 권한 확인

--- contracts/MedicalRecord.sol
+++ contracts/MedicalRecord.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.15;
 
 contract MedicalRecord {
+    // 세부 내역
     struct TreatmentDetail {
         string category;
         string date;
@@ -13,7 +14,7 @@ contract MedicalRecord {
         uint256 foop;
         uint256 nonReimbursement;
     }
-
+    // 접수 기록
     struct MedicalRecordData {
         string name;
         string RRN;
@@ -28,13 +29,15 @@ contract MedicalRecord {
         uint256 treatDetailsCount;
     }
 
-    // attribute
-    string private constant DEFAULT_KCD = "J09"; // 디폴트 RRN 값
+    // 속성
+    string private constant DEFAULT_KCD = "J09"; // Default KCD value
+    string private constant DEFAULT_owner = "0x28cae9e57d8dd34eb6472526f04d4c12ffa9432b";
     mapping(string => MedicalRecordData) private medicalRecords;
     uint256 public medicalRecordsCount;
-    string[] private medicalRecordKeys; 
-    
-    // event
+    string[] private medicalRecordKeys;
+    mapping(string => address) private recordOwners; // Store record owners
+
+    // 이벤트 반환 구조체
     event MedicalRecordSet(
         string name,
         string RRN,
@@ -48,7 +51,8 @@ contract MedicalRecord {
         TreatmentDetail[] treatDetails
     );
 
-    // 보험사 청구 데이터 저장
+    // 메소드
+    // 새로운 접수 기록 저장
     function addMedicalRecord(
         string memory _name,
         string memory _RRN,
@@ -79,25 +83,81 @@ contract MedicalRecord {
             record.treatDetails.push(_treatDetails[i]);
         }
         record.treatDetailsCount = _treatDetails.length;
-        
-        // getter를 위해
-				medicalRecordKeys.push(_receiptNumber);
-				medicalRecordsCount++;
-        
+
+        medicalRecordKeys.push(_receiptNumber);
+        medicalRecordsCount++;
+        recordOwners[_receiptNumber] = msg.sender;
+
         emit MedicalRecordSet(_name, _RRN, _KCD, _date, _receiptNumber, _totalOop, _totalPcc, _totalFoop, _nonReimbursement, _treatDetails);
     }
-  
 
+    // 영수 번호 기준 조회
     function getMedicalRecord(string memory _receiptNumber) public view returns (MedicalRecordData memory) {
         return medicalRecords[_receiptNumber];
     }
-    
-    function getMedicalRecords() public view returns (MedicalRecordData[] memory){
-		    MedicalRecordData[] memory records = new MedicalRecordData[](medicalRecordsCount);
-				for (uint256 i = 0; i < medicalRecordKeys.length; i++) {
-						records[i] = medicalRecords[medicalRecordKeys[i]];
-				}
-				return records;
+
+    // 모든 접수 기록 조회
+    function getMedicalRecords() public view returns (MedicalRecordData[] memory) {
+        MedicalRecordData[] memory records = new MedicalRecordData[](medicalRecordsCount);
+        for (uint256 i = 0; i < medicalRecordKeys.length; i++) {
+            records[i] = medicalRecords[medicalRecordKeys[i]];
+        }
+        return records;
+    }
+
+    // 모든 접수 기록 페이징 처리, 
+    function getPagedMedicalRecords(uint256 page, uint256 pageSize, uint256 curCount) public view returns (MedicalRecordData[] memory) {
+        if (pageSize == 0 || page == 0) {
+            return new MedicalRecordData[](0);
+        }
+
+        if ((page - 1) * pageSize >= curCount) {
+            return new MedicalRecordData[](0);
+        }
+
+        uint256 startIndex = curCount - (page - 1) * pageSize - 1;
+        uint256 endIndex = startIndex >= (pageSize - 1) ? startIndex - (pageSize - 1) : 0;
+
+        uint256 count = startIndex - endIndex + 1;
+        MedicalRecordData[] memory pageRecords = new MedicalRecordData[](count);
+        for (uint256 i = 0; i < count; i++) {
+            pageRecords[i] = medicalRecords[medicalRecordKeys[startIndex - i]];
+        }
+        return pageRecords;
+    }
+
+    // 접수 기록 삭제
+    function deleteMedicalRecord(string memory _receiptNumber) public {
+        require(bytes(medicalRecords[_receiptNumber].name).length != 0, "No record found to delete.");
+        require(isOwner(), "Unauthorized access.");
+
+        delete medicalRecords[_receiptNumber];
+        removeKey(_receiptNumber);
+    }
+
+    // key값 삭제
+    function removeKey(string memory _receiptNumber) private {
+        uint256 index = findIndex(_receiptNumber);
+        if (index < medicalRecordKeys.length - 1) {
+            medicalRecordKeys[index] = medicalRecordKeys[medicalRecordKeys.length - 1];
+        }
+        medicalRecordKeys.pop();
+        medicalRecordsCount--;
+    }
+
+    // key값 위치 확인
+    function findIndex(string memory _receiptNumber) private view returns (uint256) {
+        for (uint256 i = 0; i < medicalRecordKeys.length; i++) {
+            if (keccak256(bytes(medicalRecordKeys[i])) == keccak256(bytes(_receiptNumber))) {
+                return i;
+            }
+        }
+        revert("Receipt number not found.");
+    }
+
+    // 권한 확인
+    function isOwner() public view returns (bool) {
+	return keccak256(abi.encodePacked(msg.sender)) == keccak256(abi.encodePacked(DEFAULT_owner));
     }
 }
 
